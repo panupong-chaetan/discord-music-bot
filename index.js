0ba2219d7a73985bb377bfba3d0ab6ffe69979a2
Panupong Chaetan <panupong.chaetan@gmail.com>
2025-08-13 17:48:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
-const GAS_WEBAPP_URL = process.env.GAS_WEBAPP_URL;
+const GAS_WEBAPP_URL_BOSS = process.env.GAS_WEBAPP_URL_BOSS;
+const GAS_WEBAPP_URL_EVENT = process.env.GAS_WEBAPP_URL_EVENT;
 
 const client = new Client({
   intents: [
@@ -26,7 +27,7 @@ client.on('messageCreate', async (message) => {
     try {
       await message.channel.send('กำลังดึงเวลาบอสทั้งหมด... โปรดรอสักครู่');
 
-      const response = await fetch(GAS_WEBAPP_URL, {
+      const response = await fetch(GAS_WEBAPP_URL_BOSS, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ content: message.content }),
@@ -38,6 +39,24 @@ client.on('messageCreate', async (message) => {
     } catch (error) {
       console.error('❌ Error sending to GAS:', error);
       await message.channel.send('เกิดข้อผิดพลาดในการดึงข้อมูลบอส');
+    }
+  }
+  else if (message.content.includes('เวลากิจกรรม!')) {
+    try {
+      await message.channel.send('กำลังดึงเวลากิจกรรมทั้งหมด... โปรดรอสักครู่');
+
+      const response = await fetch(GAS_WEBAPP_URL_EVENT, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ content: message.content }),
+      });
+
+      const text = await response.text();
+      await message.channel.send(text);
+
+    } catch (error) {
+      console.error('❌ Error sending to GAS:', error);
+      await message.channel.send('เกิดข้อผิดพลาดในการดึงข้อมูลกิจกรรม');
     }
   }
 });
